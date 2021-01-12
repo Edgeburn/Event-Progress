2eb3d81da00a72a032ebeffc733a51ab91bdf311
Connor Watson <connorwatson002@outlook.com>
2021-01-12 15:41:49 -0500

    moved file loading into `reloadEvts()` function

--- main.py
+++ main.py
@@ -9,20 +9,7 @@ from miscfuncs import *
 
 clearTerminal()
 
-events = []  # Main events list
-
-# Read all files in current directory for processing
-eventFileNames = []
-
-with os.scandir() as filesInDirectory:  # Get all files in directory, and load their filenames into a list
-    for file in filesInDirectory:
-        eventFileNames.append(file.name)
-
-eventFileNames = list(filter(isEbmevtFile, eventFileNames))  # Ignore files which aren't EventProgress files
-
-for i in range(len(eventFileNames)):  # Generate CalEvent objects for each file
-    currentFilename = eventFileNames[i]
-    events.append(CalEvent(currentFilename, i))
+events = reloadEvts()
 
 
 appIsRunning = False
